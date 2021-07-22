d8e3e6d5ec03c9966816b79fac5bf5c5f232c65c
ertimofeev <58752172+ertimofeev@users.noreply.github.com>
2021-07-22 23:58:04 +0300

    Add files via upload

--- detection.py
+++ detection.py
@@ -10,107 +10,67 @@ from PIL import Image, ImageFile
 from openvino.inference_engine import IECore
 import argparse
 
-def scale_tensor(img_tensor, default_scale=256):
-    _, _, w, h = img_tensor.shape
-    print(w, h)
-    if w < h:
-        ow = default_scale
-        oh = h / w * default_scale
-    else:
-        oh = default_scale
-        ow = w / h * default_scale
-
-    oh = int(round(oh / 16) * 16)
-    ow = int(round(ow / 16) * 16)
-
-    return F.interpolate(img_tensor, [ow, oh], mode="bilinear")
 
 def main(config):
-    #models_shape
-    scratch_h, scratch_w = (608,512)
-    inp_h, inp_w = (680, 512)
-    
     #image_for_models
-    input = cv2.imread(config.input_path)
-    
+    img = cv2.imread(config.input_path)
     ie = IECore()
 
     #scratch_detection
-    model = config.model
-    net = ie.read_network(model=model)
-    assert len(net.input_info.keys()) == 1
-    assert len(net.outputs) == 1
-    input_blob = next(iter(net.input_info))
-    out_blob = next(iter(net.outputs))
-    net.reshape({input_blob: (1, 1, scratch_w, scratch_h)})          
-    if config.GPU >= 0:
-        exec_net = ie.load_network(network=net, device_name="GPU")
-    else:
-        exec_net = ie.load_network(network=net, device_name="CPU")
-
-    #prepare_info
-    scratch_image = os.path.join(config.input_path)
-    scratch_image = Image.open(scratch_image).convert("RGB")
-    w, h = scratch_image.size
-    transformed_image_PIL = scratch_image.resize((scratch_h, scratch_w), Image.BICUBIC)
-    scratch_image = transformed_image_PIL.convert("L")
-    scratch_image = tv.transforms.ToTensor()(scratch_image)
-    scratch_image = tv.transforms.Normalize([0.5], [0.5])(scratch_image)
-    scratch_image = torch.unsqueeze(scratch_image, 0)
-    print(scratch_image.shape)
     
-    with torch.no_grad():
-        scratch_res = exec_net.infer(inputs={input_blob: scratch_image})
-        scratch_res = scratch_res[out_blob]
-        
-        mask = scratch_res[0]
-        mask = mask.transpose((1, 2, 0))
-        print(1)
-        scratch_output = cv2.resize(input, (scratch_h, scratch_w), interpolation = cv2.INTER_CUBIC)
-        for i in range(scratch_w):
-            for j in range (scratch_h):
-                scratch_res[0,0,i,j] = 1/(1+np.exp(-mask[i,j,0]))
-                if scratch_res[0,0,i,j]>0.4:
-                    scratch_res[0,0,i,j] = 1
-                    scratch_output[i,j] = [255,255,255]
-                else:
-                    scratch_res[0,0,i,j] = 0
-                    scratch_output[i,j] = [0,0,0]
-        print(2)
-        #save_mask
-        cv2.imwrite(config.output_dir+"\\mask\\1.png", scratch_output)
-    print(3)
-    #inpainting
-    inp_model = config.inpainting_model
-    inp_net = ie.read_network(model=inp_model)
-    inp_input_blob = next(iter(inp_net.input_info))
-    inp_out_blob = next(iter(inp_net.outputs))
-    if config.GPU >= 0:
-        inp_exec_net = ie.load_network(network=inp_net, device_name="GPU")
-    else:
-        inp_exec_net = ie.load_network(network=inp_net, device_name="CPU")
-    print(4)
+    net_1 = ie.read_network(model=config.scratch_model)
+    exec_net_1 = ie.load_network(network=net_1, device_name="CPU")
+    
     #prepare_info
-    #inpainting_input
-    inp_input = cv2.resize(input, (inp_h, inp_w), interpolation = cv2.INTER_CUBIC)
-    inp_input = inp_input.transpose((2, 0, 1))
-    #mask
-    inp_mask = scratch_res
-    inp_mask = inp_mask[0,0]
-    inp_mask = cv2.resize(inp_mask, (inp_h, inp_w), interpolation=cv2.INTER_CUBIC)
-    inp_mask=inp_mask.astype(int)
-    inp_mask = [[inp_mask]]
-    print(5)
-    inp_res = inp_exec_net.infer(inputs={inp_input_blob: inp_input, inp_input_blob+"_1": inp_mask})
-    inp_res = inp_res[inp_out_blob]
-    inp_res = inp_res[0].transpose((1, 2, 0))
-    print(6)
-    #save_image
-    cv2.imwrite(config.output_dir+"\\result\\1.png", inp_res)
-    print(7)
-    cv2.imshow("input", cv2.resize(input, (inp_h, inp_w), interpolation = cv2.INTER_CUBIC))
-    cv2.imshow("mask", scratch_output)
-    cv2.imshow("result", inp_res)
+    data_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    norm_data = data_gray.astype(np.float32) - 127.5
+    norm_data /= 127.5
+    resized_data = cv2.resize(norm_data, (608, 512))
+    scratch_input = np.array([[resized_data]])
+    out = exec_net_1.infer(inputs = {'input': scratch_input})
+    
+    mask = np.squeeze(out['output'])
+    mask[mask >= 0.5] = 1.
+    mask[mask < 0.5] = 0.
+    #res_mask = mask.copy()
+    h,w, = mask.shape
+    for i in range(int(h/2-200),int(h/2-140),1):
+        for j in range(int(w/2-30),int(w/2+30),1):
+            mask[i,j]=1
+           #if mask[i,j] == 1:
+           #     for y in range(-5,5,1):
+           #         for x in range(-5,5,1):
+           #             res_mask[i+y,j+x]=1
+    #mask = res_mask
+    cv2.imwrite(config.output_dir+"\\mask\\8.png", mask*255)
+    
+    #inpainting
+    if mask.max() == 1:
+        net_2 = ie.read_network(model=config.inpainting_model)
+        exec_net_2 = ie.load_network(network=net_2, device_name="CPU")
+    
+        #prepare_info
+        resized_img = cv2.resize(img, (680, 512)).astype(np.float32)
+        resized_mask = cv2.resize(mask, (680, 512)).astype(np.float32)
+
+        resized_mask[resized_mask >= 0.5] = 1
+        resized_mask[resized_mask < 0.5] = 0
+        if len(resized_mask.shape) == 2:
+            resized_mask = np.expand_dims(resized_mask, -1)
+        resized_img = resized_img * (1 - resized_mask) + 255 * resized_mask
+        inpainting_input = np.expand_dims(resized_img.transpose(2, 0, 1), 0)
+        inpainting_mask = np.expand_dims(resized_mask.transpose(2, 0, 1), 0)
+        print(inpainting_mask.shape)
+
+        out = exec_net_2.infer(inputs = {'Placeholder': inpainting_input, 'Placeholder_1': inpainting_mask})
+        print(next(iter(net_2.outputs)))
+        out_img = out['Minimum'][0].transpose((1, 2, 0)).astype(np.uint8)
+    
+        #save_image
+        cv2.imwrite(config.output_dir+"\\result\\8.png", out_img)
+        cv2.imshow("input", cv2.resize(img, (680, 512), interpolation = cv2.INTER_CUBIC))
+        cv2.imshow("out_img", out_img)
+    cv2.imshow("mask", mask*255)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -119,8 +79,8 @@ def main(config):
 if __name__ == "__main__":
         parser = argparse.ArgumentParser()
         parser.add_argument("--GPU", type=int, default=-1)
-        parser.add_argument("--model", type=str, default="scratch_detector.xml")
-        parser.add_argument("--input_path", type=str, default="my\\2.png")
+        parser.add_argument("--scratch_model", type=str, default="scratch_detector.xml")
+        parser.add_argument("--input_path", type=str, default="my\\8.jpeg")
         parser.add_argument("--output_dir", type=str, default="restored_image")
         parser.add_argument("--input_size", type=str, default="scale_256", help="resize_256|full_size|scale_256")
         parser.add_argument("--inpainting_model", type=str, default="gmcnn-places2-tf.xml")
